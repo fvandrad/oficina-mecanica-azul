3c5bc2a24943d1c485d912b90d4694f89d0749e6
Fernando de Andrade <fvandrad@gmail.com>
2025-05-27 10:39:27 -0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/oficina-mecanica-azul-www/",
+  base: "/oficina-mecanica-azul/",
   plugins: [vue()],
   server: {
     host: true,
